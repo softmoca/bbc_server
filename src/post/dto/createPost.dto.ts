f66b416f146621f8dc3d1742abd5cb6c20019aba
softmoca <acyc0909@gmail.com>
2023-10-21 13:32:15 +0900

    feat : postDto  minlength max length

--- src/post/dto/createPost.dto.ts
+++ src/post/dto/createPost.dto.ts
@@ -1,12 +1,21 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreatePostDto {
+  @MinLength(4)
+  @MaxLength(20)
   @IsNotEmpty()
   postTitle: string;
+
+  @MinLength(1)
+  @MaxLength(200)
   @IsNotEmpty()
   postContent: string;
+
   @IsNotEmpty()
   buildingName: string;
+
+  @MinLength(4)
+  @MaxLength(20)
   @IsNotEmpty()
   chatRoomTitle: string;
 }
