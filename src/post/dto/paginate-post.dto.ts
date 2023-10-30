065dddbccf6d44ea9432c7f12a4174597547b95e
softmoca <acyc0909@gmail.com>
2023-10-30 19:47:41 +0900

    feat : PaginatePostDTo

---
+++ src/post/dto/paginate-post.dto.ts
@@ -0,0 +1,22 @@
+import { IsIn, IsNumber, IsOptional } from 'class-validator';
+export class PaginatePostDto {
+  @IsNumber()
+  @IsOptional()
+  page?: number;
+
+  @IsNumber()
+  @IsOptional()
+  where__id__less_than?: number;
+
+  @IsNumber()
+  @IsOptional()
+  where__id__more_than?: number;
+
+  @IsIn(['ASC', 'DESC'])
+  @IsOptional()
+  order__createdAt: 'DESC' | 'ASC' = 'DESC';
+
+  @IsNumber()
+  @IsOptional()
+  take: number = 20;
+}
