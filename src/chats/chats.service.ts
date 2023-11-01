d2fe0735147e98a39cda01e73272dbd1720f94c6
softmoca <acyc0909@gmail.com>
2023-11-01 23:09:16 +0900

    feat : create creatChat Service

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { Chats } from 'src/entities/chats.entity';
+import { CreateChatDto } from './dto/create-chat.dto';
 
 @Injectable()
-export class ChatsService {}
+export class ChatsService {
+  constructor(
+    @InjectRepository(Chats)
+    private readonly chatsRepository: Repository<Chats>,
+  ) {}
+
+  async createChat(dto: CreateChatDto) {
+    const chat = await this.chatsRepository.save({
+      users: dto.userIds.map((id) => ({ id })),
+    });
+
+    return this.chatsRepository.findOne({
+      where: {
+        id: chat.id,
+      },
+    });
+  }
+}
