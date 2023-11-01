c1bba390e491916423c895bd656d162cd7afafc3
softmoca <acyc0909@gmail.com>
2023-11-01 23:23:24 +0900

    feat : creat creatchat gateway

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -7,16 +7,28 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { CreateChatDto } from './dto/create-chat.dto';
+import { ChatsService } from './chats.service';
 
 @WebSocketGateway({
   namespace: 'chats', //ws:localhost:3000/chats
 })
 export class ChatsGateway implements OnGatewayConnection {
+  constructor(private readonly chatsService: ChatsService) {}
+
   @WebSocketServer()
   server: Server;
 
   handleConnection(socket: Socket) {
     console.log(`on connet called : ${socket.id}`);
+  }
+
+  @SubscribeMessage('create_chat')
+  async createChat(
+    @MessageBody() data: CreateChatDto,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const chat = await this.chatsService.createChat(data);
   }
 
   @SubscribeMessage('enter_chat')
