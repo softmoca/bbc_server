9a7318856f29545278a4086fa9b2481c32176b76
softmoca <acyc0909@gmail.com>
2023-11-01 22:42:35 +0900

    feat :server send event

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -3,19 +3,23 @@ import {
   OnGatewayConnection,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
   namespace: 'chats', //ws:localhost:3000/chats
 })
 export class ChatsGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Server;
+
   handleConnection(socket: Socket) {
     console.log(`on connet called : ${socket.id}`);
   }
 
   @SubscribeMessage('send_message')
   sendMessage(@MessageBody() message: string) {
-    console.log(message);
+    this.server.emit('receive_message', '서버로 부터온 메세지');
   }
 }
